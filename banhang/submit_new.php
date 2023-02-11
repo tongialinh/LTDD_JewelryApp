9ec4d0ba0cfa1520845ec06d2334c04e5026f271
Ton Gia Linh <95701446+tongialinh@users.noreply.github.com>
2023-02-11 22:23:34 +0700

    banhang
    update data

---
+++ banhang/submit_new.php
@@ -0,0 +1,19 @@
+<?php
+  include "connect.php";
+
+if(isset($_POST['submit_password']) && $_POST['email'])
+{
+  $email=$_POST['email'];
+  $pass=$_POST['password'];
+
+  $query = "update user set pass='$pass' where email='$email'";
+  //print_r($query);
+  $data = mysqli_query($conn, $query);
+
+   if($data == true)
+  {
+    echo "Đổi mật khẩu thành công";
+  }
+  
+}
+?>
